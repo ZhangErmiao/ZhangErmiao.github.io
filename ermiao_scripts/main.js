4a931a454b409e34179ce7e3bc31522e66b58109
ZhangErmiao <35625334+ZhangErmiao@users.noreply.github.com>
2018-11-02 17:11:50 +0800

    update

--- ermiao_scripts/main.js
+++ ermiao_scripts/main.js
@@ -1,29 +1,12 @@
-var myImage = document.querySelector('img');
-
-myImage.onclick = function(){
-  var mySrc = myImage.getAttribute('src')
-  if(mySrc === 'images/cat.jpg'){
-    myImage.setAttribute('src','images/cat2.jpg');
-  }
-  else{
-    myImage.setAttribute('src','images/cat.jpg')
-  }
+var my_right_button = document.querySelector("button[name='right']");
+var my_left_button = document.querySelector("button[name='left']");
+function open_hidden_tree(){
+  my_tree = document.querySelector("#hidden_tree");
+  my_tree.style.display = "block";
 }
-
-var myButton = document.querySelector('button');
-var myHeading = document.querySelector('h1');
-function setUserName(){
-  var myName = prompt('please enter your name.');
-  localStorage.setItem('name',myName);
-  myHeading.textContent = 'Mozilla is cool,' + myName;
+function close_hidden_tree(){
+  my_tree = document.querySelector("#hidden_tree");
+  my_tree.style.display = "none";
 }
-if(!localStorage.getItem('name')){
-  setUserName();
-}
-else{
-  var storedName = localStorage.getItem('name');
-  myHeading.textContent = 'Mozilla is cool,'+storedName;
-}
-myButton.onclick = function(){
-  setUserName();
-}
+my_right_button.addEventListener('click',open_hidden_tree, false);
+my_left_button.addEventListener('click',close_hidden_tree, false);
